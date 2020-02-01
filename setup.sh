693b152f9031e5405c362ccc579ba0845bf6bc89
b0dian <contact@dimitriquenault.com>
2020-02-02 00:57:02 +0100

    Added yay install

--- setup.sh
+++ setup.sh
@@ -6,7 +6,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Necessaries packages
 
-#yay -S neovim tmux
+pacman -S neovim tmux
+
+# Yay installation for yaourt packages
+
+git clone https://aur.archlinux.org/yay.git ~/yay
+
+cd ~/yay
+
+makepkg -si
 
 # Symlink bash config file
 
@@ -14,7 +22,7 @@ ln -sf $DIR/bashrc ~/.bashrc
 
 # Symlink tmuux config file
 
-[ -e ~/.tmux.conf ] || ln -s $DIR/tmux.conf ~/.tmux.conf
+[ -e ~/.tmux.conf ] || ln -sf $DIR/tmux.conf ~/.tmux.conf
 
 # check config directory is present or create it
 
